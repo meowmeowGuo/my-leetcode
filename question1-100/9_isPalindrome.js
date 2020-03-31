07a636ec4c4371e26b9b4c52a819dfed94d71b0c
miaomiaoG <15827061459@163.com>
2020-03-31 10:10:14 +0800

    #9 回文数字

---
+++ question1-100/9_isPalindrome.js
@@ -0,0 +1,40 @@
+/*
+ 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ 示例 1:
+ 输入: 121
+ 输出: true
+ 示例 2:
+ 输入: -121
+ 输出: false
+ 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * */
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function (x) {
+  return parseInt(String(x).split('').reverse().join('')) === x;
+};
+
+/* 不转换字符串实现 */
+
+const isPalindrome2 = function (x) {
+  if (x < 0) {
+    return false;
+  }
+  if (x < 10) {
+    return true;
+  }
+  let power = 10;
+  const restArray = [];
+  let temp = x;
+  do {
+    restArray.push(temp % power);
+    temp = Math.floor(temp / power);
+  } while (temp);
+  console.log(parseInt(restArray.reverse().join('')));
+  return parseInt(restArray.join('')) === x;
+};
+
+a = 10;
+console.log(isPalindrome2(a));
